cd18648d9cafb942d2a883d23846dd7b81b974a9
toan.le <toanlv@smartosc.com>
2020-07-14 13:42:58 +0700

    update

---
+++ src/presentation/ProductList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="product-app">
+      <h2>{{getName('PRoduct App')}}</h2>
+      <button @click="changeName('New Name')">Change Name</button>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
+<script lang="ts">
+import Vue from 'vue';
+interface IData {}
+export default Vue.extend({
+  components: {
+    
+  },
+  data(){
+    return {
+      name: 'Product List'
+    }
+  },
+  created(){
+    this.name = 'Product Created'
+  },
+  mounted(){
+    this.name = 'Product Detail'
+  },
+  methods: {
+    getName(name: string){
+      this.name = name;
+      return this.name;
+    },
+    changeName(name: string){
+      this.$emit('changeMyName');
+    }
+  }
+})
+</script>
---
+++ src/presentation/Products.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="product-app">
+     <ProductList :changeMyName="changeName"/>
+  </div>
+</template>
+
+<style lang="scss">
+#product-app {
+    min-height: 300px;
+}
+</style>
+<script lang="ts">
+import ProductList from './ProductList.vue';
+interface IData {}
+export default {
+      components: {
+    ProductList
+  },
+    methods: {
+        changeName(){
+            alert('new name')
+        }
+    }
+
+}
+</script>
